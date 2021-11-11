f0f75103f0c16884fcb5ae865a5a0b9d2b770396
Oleksii Babanskyi <coder95@i.ua>
2021-11-11 15:46:17 +0200

    Fixed cart products number in mobile screen

--- catalog/language/ru-ru/common/cart.php
+++ catalog/language/ru-ru/common/cart.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_items']     = '<span>%s</span>%s';
+$_['text_items']     = '<span>%s</span><span class="cart-price">%s</span>';
 $_['text_empty']     = 'Ваша корзина пуста!';
 $_['text_cart']      = 'Перейти в корзину';
 $_['text_checkout']  = 'Оформить заказ';
